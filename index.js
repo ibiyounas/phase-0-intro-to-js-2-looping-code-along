8a8368326ff04a6d869531ea1bc04a63d01db2af
Ibrahem Younas <younasibrahem@gmail.com>
2024-03-18 16:08:23 -0400

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Guadelupe", "Ollie", "Aki",];
+
+function writeCards(names, events) {
+    let thankYous = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+        
+    }
+return thankYous;
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int--);
+    }
+}
